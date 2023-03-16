9295b4d88e44d81248bcb4ac9bb7b123dbc79773
angel <angel_apineda@hotmail.com>
2023-03-16 00:17:55 -0600

    Commit inicial pw6

---
+++ Postwork_S6/Postwork_S6/src/test/java/org/bedu/backend/postwork_s6/PostworkS6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.backend.postwork_s6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostworkS6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
